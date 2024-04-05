cdd4a39544d918402524d09bfae1dd288d172599
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-04-05 13:30:25 +0800

    feat: api to connect frontend to backend

---
+++ front-end/src/redux/usersApiSlice.js
@@ -0,0 +1,36 @@
+import { apiSlice } from "./apiSlice"
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `/users/login`,
+        method: "POST",
+        body: data
+      }) 
+    }),
+
+    logout: builder.mutation({
+      query: () => ({
+        url: `/users/logout`,
+        method: "POST",
+      })
+    }),
+
+    register: builder.mutation({
+      query: (data) => ({
+        url: `/users/register`,
+        method: "POST",
+        body: data,
+      })
+    })
+
+
+  })
+})
+
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation
+} = userApiSlice

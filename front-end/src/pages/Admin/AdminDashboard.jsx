9dd0a9880890f36db1054ccda811b480bccecc35
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-04-07 13:26:38 +0800

    Update admin dashboard responsiveness

--- front-end/src/pages/Admin/AdminDashboard.jsx
+++ front-end/src/pages/Admin/AdminDashboard.jsx
@@ -11,21 +11,21 @@ function AdminDashboard() {
         <div className="flex justify-between mx-4 flex-wrap ">
           <h1 className="font-bold text-2xl mt-5">Welcome back, Admin.</h1>
         </div>
-        <div className="flex flex-wrap gap-6 px-2 my-3">
+        <div className="flex flex-wrap justify-center items-center gap-6 my-3">
           <Total title="Total Applicants" total="163" added="+" />
           <Total title="CV Passing Applicants" total="106" added="+" />
           <Total title="Passed Assessment" total="56" added="+" />
           <Total title="Total Passing" total="23" added="+" />
         </div>
-        <div className="flex justify-center gap-7 flex-wrap my-7 mx-5">
-          <div className="p-5 border-2 rounded-lg shadow-md">
-            <p className="text-xl font-bold mb-5">
+        <div className="flex justify-center gap-7 flex-wrap my-7">
+          <div className="p-5 border-2 rounded-lg shadow-md flex flex-col justify-center items-center">
+            <p className="text-xl font-bold mb-5 text-center">
               Passed Curriculum Vitae&apos;s
             </p>
             <TablePassing />
           </div>
           <div className="p-5 border-2 rounded-lg shadow-md">
-            <p className="text-xl font-bold mb-3">Passed Assessments</p>
+            <p className="text-xl trfont-bold mb-3">Passed Assessments</p>
             <Top />
           </div>
         </div>

c3c885838037d995f95a5b30ef71a02130c7455b
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-04-07 13:26:14 +0800

    Modify the positioning

--- front-end/src/components/TablePassing.jsx
+++ front-end/src/components/TablePassing.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Container from './Container';
 import {
   Table,
   TableBody,
@@ -14,34 +15,20 @@ import {Link} from 'react-router-dom'
 const invoices = [
   {
     invoice: "INV001",
-    CV: "Success",
+    CV: "Passed",
     assessment: "Failed",
     name: "Fabella, Emmanuel T.",
     totalAmount: "55%",
   },
   {
     invoice: "INV002",
-    CV: "Pending",
-    assessment: "Failed",
-    name: "Raizen Vahn Cedrick Sanchez",
-    totalAmount: "25%",
-  },
-  {
-    invoice: "INV003",
-    CV: "Failed",
-    name: "Marcus Zach Lestat Guttierez Rancio",
-    assessment: "Success",
-    totalAmount: "65%",
-  },
-  {
-    invoice: "INV004",
-    CV: "Success",
+    CV: "Passed",
     assessment: "Failed",
     name: "Fabella, Emmanuel T.",
     totalAmount: "55%",
   },
   {
-    invoice: "INV005",
+    invoice: "INV003",
     CV: "Pending",
     assessment: "Failed",
     name: "Raizen Vahn Cedrick Sanchez",
@@ -49,20 +36,6 @@ const invoices = [
   },
   {
     invoice: "INV006",
-    CV: "Failed",
-    name: "Marcus Zach Lestat Guttierez Rancio",
-    assessment: "Success",
-    totalAmount: "65%",
-  },
-  {
-    invoice: "INV007",
-    CV: "Success",
-    assessment: "Failed",
-    name: "Fabella, Emmanuel T.",
-    totalAmount: "55%",
-  },
-  {
-    invoice: "INV008",
     CV: "Pending",
     assessment: "Failed",
     name: "Raizen Vahn Cedrick Sanchez",
@@ -70,36 +43,36 @@ const invoices = [
   },
   {
     invoice: "INV009",
-    CV: "Failed",
-    name: "Marcus Zach Lestat Guttierez Rancio",
-    assessment: "Success",
-    totalAmount: "65%",
-  },
+    CV: "Pending",
+    assessment: "Failed",
+    name: "Raizen Vahn Cedrick Sanchez",
+    totalAmount: "25%",
+  }
 ]
  
 const TablePassing = () => {
   return (
-    <Table className="w-full md:w-full lg:w-[850px] ">
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Job</TableHead>
-          <TableHead className="w-[100px]">CV</TableHead>
-          <TableHead className="w-[200px]">Name</TableHead>
-          <TableHead className="w-[70px]">CV Rating</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.CV}</TableCell>
-            <TableCell><Link to="/view-user">{invoice.name}</Link></TableCell>
-            <TableCell className="[200px]">{invoice.totalAmount}</TableCell>
+      <Table className="w-full md:w-full lg:w-[850px] relative right-5 p-0">
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Job</TableHead>
+            <TableHead className="w-[100px]">CV</TableHead>
+            <TableHead className="w-[200px]">Name</TableHead>
+            <TableHead className="w-[70px]">CV Rating</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell>{invoice.CV}</TableCell>
+              <TableCell><Link to="/view-user">{invoice.name}</Link></TableCell>
+              <TableCell className="[200px]">{invoice.totalAmount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
   )
 }
 

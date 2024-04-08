f01eee0b2cf22978fbcd9ccc4420ac954487a9cd
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-04-08 09:23:21 +0800

    Reroute components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,58 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import Login from "../src/components/Login.jsx";
+import { useState, createContext } from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+// Global Access
+import LoginPage from "@/pages/LoginPage.jsx";
 import SignUpPage from "@/pages/SignUpPage.jsx";
 import NotFoundPage from "@/pages/NotFoundPage.jsx";
 import MainLayout from "@/layout/MainLayout.jsx";
-import JobPage from "./pages/JobPage.jsx";
+
+// Global Access && Authenticated
+import JobPage, {jobLoader} from "./pages/JobPage.jsx";
+
+// Applicant Side Import
 import ApplicantDashboardPage from "./pages/Applicant/ApplicantDashboardPage.jsx";
 import ApplicantProfilePage from "./pages/Applicant/ApplicantProfilePage.jsx";
+import ViewUser from "./pages/Admin/ViewUser.jsx";
+
+// Applicant Assessment
 import AssessmentDashboard from "./pages/Applicant/Assessment/AssessmentDashboard.jsx";
-import Assessmentchoices from "./pages/Applicant/Assessment/Assessmentchoices.jsx";
-import Assessment from "./pages/Applicant/Assessment/Assessment.jsx";
+import Assessmentchoices from "./pages/Applicant/Assessment/Assessmentchoices.jsx"
+import Assessment from "./pages/Applicant/Assessment/Assessment.jsx"
+import AssessmentCognitive from "./pages/Applicant/Assessment/AssessmentCognitive.jsx";
+
+// Admin Side Import
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 import AddJobPage from "./pages/Admin/AddJobPage.jsx";
 import EditJobPage from "./pages/Admin/EditJobPage.jsx";
-import ViewUser from './pages/Admin/ViewUser.jsx';
+export const HideAssessmentContext = createContext();
+
+// Theming Import
 import { ThemeProvider } from "./components/theme-provider.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAkRNKXJVBa--Pp-PILg8-0T_OtdSpFMlo",
-  authDomain: "artisan-peeper.firebaseapp.com",
-  projectId: "artisan-peeper",
-  storageBucket: "artisan-peeper.appspot.com",
-  messagingSenderId: "122178031252",
-  appId: "1:122178031252:web:a50cc6498077d213cbae81",
-  measurementId: "G-CS97LJ6LLY"
-};
-
-const adminUid = "H8cUeg6QAxVV5xTpJMHePhdZIfI2"; // Admin UID
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAssessmentOpen, setIsAssessmentOpen] = useState(true);
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const isAdminUser = user.uid === adminUid;
-        setIsAdmin(isAdminUser);
-        setIsAuthenticated(true);
-      } else {
-        setIsAdmin(false);
-        setIsAuthenticated(false);
-      }
-      setLoading(false);
-    });
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAssessmentOpen, setIsAssessmentOpen] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
 
-    return () => unsubscribe();
-  }, []);
-
+  // Add New Job
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -64,6 +53,7 @@ function App() {
     return;
   }
 
+  // Delete Job
   const deleteJob = async (id) => {
     const res = await fetch(`/api/jobs/${id}`, {
       method: 'DELETE'
@@ -71,6 +61,7 @@ function App() {
     return;
   }
 
+  // Update Job
   const updateJob = async (job) => {
     const res = await fetch(`/api/jobs/${job.id}`, {
       method: 'PUT',
@@ -82,39 +73,85 @@ function App() {
     return;
   }
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const router = createBrowserRouter (
+    createRoutesFromElements (
+      <>
+        {/* Authentication */}
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<SignUpPage/>}/>
 
-  return (
-    <ThemeProvider>
-      <BrowserRouter>
-        {!isAuthenticated && <Navigate to="/login" />}
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<SignUpPage />} />
-          <Route path="/" element={<MainLayout isAdmin={isAdmin} isAssessmentOpen={isAssessmentOpen} />}>
-            <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} />
-            <Route path="/dashboard" element={<ApplicantDashboardPage />} />
-            <Route path="/profile" element={<ApplicantProfilePage />} />
-            <Route path="/assessment-dashboard" element={<AssessmentDashboard />} />
-            <Route path="/assessment-areas" element={<Assessmentchoices />} />
-            <Route path="/assessment-start" element={<Assessment />} />
-            <Route path="/assessment-start/:id" element={<Assessment />} />
-            {isAdmin && (
+        {/* Authentication Global Access */}
+        {isAuthenticated ? (
+          <>
+            <Route path='/' element={<MainLayout isAssessmentOpen={isAssessmentOpen}/>}>
+              <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+            </Route>
+          </>
+        ) : (
+          ''
+        )}
+
+        {/* Applicant Authentication */}
+        {isAuthenticated && !isAdmin ? (
+          <>
+            <Route path='/' element={<MainLayout isAssessmentOpen={isAssessmentOpen}/>}>
               <>
-                <Route path='/adminDashboard' element={<AdminDashboard />} />
-                <Route path='/view-user' element={<ViewUser />} />
-                <Route path='/add-job' element={<AddJobPage />} />
-                <Route path="/edit-job/:id" element={<EditJobPage />} />
+                <Route path='/dashboard' element={<ApplicantDashboardPage/>}/>
+                <Route path='/profile' element={<ApplicantProfilePage/>}/>
               </>
-            )}
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+            </Route>
+          </>
+        ) : (
+          ''
+        )}
+
+        {/* Admin Authentication */}
+        {isAuthenticated && isAdmin ? (
+          <>
+            <Route path='/' element={<MainLayout isAdmin={isAdmin}/>}>
+              <>
+                <Route path='/dashboard' element={<AdminDashboard/>}/>
+                <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
+                <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
+                <Route path='/view-user' element={<ViewUser/>}/>
+              </>
+            </Route>
+          </>
+        ) : (
+          ''
+        )}
+
+        {/* Applicant Assessment */}
+        {isAssessmentOpen && isAuthenticated ? (
+          <>
+            <Route path='/' element={<MainLayout isAssessmentOpen={isAssessmentOpen}/>}>
+              <>
+                <Route path='/assessment-dashboard' element={<AssessmentDashboard/>}/>
+                <Route path='/assessment-areas' element={<Assessmentchoices/>}/>
+                <Route path='/assessment-start' element={<Assessment/>}/>
+                <Route path='/assessment-start/:id' element={<Assessment/>}/>
+                <Route path='/assessment-cognitive' element={<AssessmentCognitive/>}/>
+              </>
+            </Route>
+          </>
+          
+        ) : (
+          ''
+        )}
+        {/* 404 Error Page */}
+        <Route path='*' element={<NotFoundPage/>}/>
+      </>
+    )
+  )
+  
+  return (
+    <>
+      {/*This is for Light/Dark Mode */}
+      <ThemeProvider> 
+        <RouterProvider router={router}/>
+      </ThemeProvider>
+    </>
   );
 }
 
-export default App;
+export default App
